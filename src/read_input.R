fe147ef1b6b4383b9cb827be9014e7e60ff64663
Idalina Sachango <idalinassachango@MacBook-Pro-4.local>
2022-09-14 15:58:35 -0500

    Removed unecessary files

--- src/read_input.R
+++
@@ -1,9 +0,0 @@
-read_input <- function(path_to_plot_folder = '', path_to_plot_script = '', ...) {
-  args = list(...)
-  
-  if (path_to_plot_folder) {
-    sapply(list.files(path_to_plot_folder, full.names = TRUE), source)
-  } else if (path_to_plot_script) {
-    source(path_to_plot_script)
-  }
-}
